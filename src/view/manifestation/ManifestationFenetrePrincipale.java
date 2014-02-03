1a85bc4b3f42f3a2c101ec46bbf8825f75913430
Aymane57 <aymane.a57@gmail.com>
2014-02-03 20:40:19 +0100

    Modification de la vue principal Ajout onglets Ajout menu Ajout Tableau statique pour la forme

--- src/view/manifestation/ManifestationFenetrePrincipale.java
+++ src/view/manifestation/ManifestationFenetrePrincipale.java
@@ -3,34 +3,45 @@ package view.manifestation;
 import controller.manifestation.ManifestationControlleurPrincipale;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 import javax.swing.WindowConstants;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 public class ManifestationFenetrePrincipale extends JFrame {
 
     public ManifestationFenetrePrincipale() {
 
-        super("Exemple d'appli MVC-DAO");
+        super("Gestion des événements");
 
         this.setJMenuBar(this.creerBarreMenu());
         this.add(creerBarreOutils(), BorderLayout.NORTH);
+        this.add(creerPanelPrincipal(), BorderLayout.CENTER);
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        this.setSize(400, 200);
+        this.setSize(700, 500);
 
         this.setVisible(true);
     }
@@ -87,7 +98,7 @@ public class ManifestationFenetrePrincipale extends JFrame {
         JToolBar jtb = new JToolBar();
         jtb.setLayout(new GridBagLayout());
         jtb.setBorder(new EmptyBorder(0, 5, 0, 0));
-        jtb.setBackground(new Color(200, 240, 240));
+        jtb.setBackground(new Color(240, 240, 240));
         ClassLoader cl = this.getClass().getClassLoader();
 
         JButton newEvent = new JButton(new ImageIcon(cl.getResource("view/images/add2.png")));
@@ -102,10 +113,23 @@ public class ManifestationFenetrePrincipale extends JFrame {
         deleteEvent.setBorderPainted(false);
         deleteEvent.setFocusPainted(false);
 
+        JButton bsearch = new JButton(new ImageIcon(cl.getResource("view/images/search.png")));
+        bsearch.setBorderPainted(false);
+        bsearch.setFocusPainted(false);
+
+        JButton xmlEvent = new JButton(new ImageIcon(cl.getResource("view/images/xml.png")));
+        xmlEvent.setBorderPainted(false);
+        xmlEvent.setFocusPainted(false);
+
+        JSeparator js = new JSeparator(JSeparator.VERTICAL);
+        js.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.LIGHT_GRAY));
+
+        JSeparator js2 = new JSeparator(JSeparator.VERTICAL);
+        js2.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.LIGHT_GRAY));
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.LINE_START;
         gbc.fill = GridBagConstraints.VERTICAL;
-        gbc.weighty = 1;
         gbc.weighty = 1;
 
         gbc.gridx = 0;
@@ -114,7 +138,7 @@ public class ManifestationFenetrePrincipale extends JFrame {
         jtb.add(newEvent, gbc);
 
         gbc.gridx = 1;
-        jtb.add(new JSeparator(JSeparator.VERTICAL), gbc);
+        jtb.add(js, gbc);
 
         gbc.gridx = 2;
         jtb.add(editEvent, gbc);
@@ -122,9 +146,92 @@ public class ManifestationFenetrePrincipale extends JFrame {
         gbc.gridx = 3;
         jtb.add(deleteEvent, gbc);
 
+        gbc.gridx = 4;
+        jtb.add(js2, gbc);
+
+        gbc.gridx = 5;
+        jtb.add(xmlEvent, gbc);
+
+        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(0, 200, 0, 5);
+        gbc.gridx = 6;
+        JTextField search = new JTextField(100);
+        jtb.add(search, gbc);
+
+        gbc.weightx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.insets = new Insets(0, 0, 0, 5);
+        gbc.gridx = 7;
+        jtb.add(bsearch, gbc);
+
         jtb.setFloatable(false);
+
+        jtb.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.LIGHT_GRAY));
 
         return jtb;
 
+    }
+
+    private JPanel creerPanelPrincipal() {
+
+        JPanel p = new JPanel();
+        p.setLayout(new GridBagLayout());
+
+        JTabbedPane tp = new JTabbedPane();
+
+        /* Partie TEST */
+        Object[][] donnees = {
+            {"1", "Journée Portes Ouvertes de l'IUT de METZ"},
+            {"2", "Journée E-Commerce à l'ENIM"},
+            {"3", "ORIACTION"},
+            {"4", "Stage en Poche"}};
+
+        String[] entetes = {"ID", "Libelle"};
+
+        JTable tableau = new JTable(donnees, entetes);
+
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc2 = new GridBagConstraints();
+
+        gbc2.anchor = GridBagConstraints.LINE_END;
+        gbc2.weightx = 1;
+        gbc2.fill = GridBagConstraints.HORIZONTAL;
+        gbc2.gridx = 0;
+        gbc2.gridy = 0;
+        tableau.getTableHeader().setBackground(new Color(245, 246, 247));
+        tableau.getTableHeader().setFont(new Font("Arial", 1, 12));
+        jp.add(tableau.getTableHeader(), gbc2);
+
+        gbc2.anchor = GridBagConstraints.LINE_END;
+        gbc2.weightx = 1;
+        gbc2.weighty = 1;
+        gbc2.fill = GridBagConstraints.BOTH;
+        gbc2.gridx = 0;
+        gbc2.gridy = 1;
+
+        jp.add(tableau, gbc2);
+
+        /* Partie TEST */
+        tp.addTab("Manifestations", jp);
+        tp.addTab("Enseignants", null);
+        tp.addTab("Etudiants", null);
+        tp.addTab("Formations", null);
+        tp.addTab("Départements", null);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.anchor = GridBagConstraints.LINE_END;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        p.add(tp, gbc);
+        return p;
     }
 }
