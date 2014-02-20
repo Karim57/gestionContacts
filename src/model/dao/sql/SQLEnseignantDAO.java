55cdb1d1ba304e044321672f0444556b2224ed44
PALERMO <anthonypalermo80@gmail.com>
2014-02-20 19:14:51 +0100

    Anthony_20/02/2014

--- src/model/dao/sql/SQLEnseignantDAO.java
+++ src/model/dao/sql/SQLEnseignantDAO.java
@@ -6,44 +6,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import model.business.Departement;
+import model.business.Enseignant;
 import model.dao.factory.MySQLDAOFactory;
 import model.dao.interfaces.DAOInterface;
 
-public class SQLDepartementDAO implements DAOInterface<Departement> {
 
-    private static SQLDepartementDAO instance = null;
+/******** Question sur l'arrayList générique***/
+public class SQLEnseignantDAO implements DAOInterface<Enseignant> {
 
-    public static SQLDepartementDAO getInstance() {
+    private static SQLEnseignantDAO instance = null;
 
-        if (SQLDepartementDAO.instance == null) {
-            SQLDepartementDAO.instance = new SQLDepartementDAO();
+    public static SQLEnseignantDAO getInstance() {
+
+        if (SQLEnseignantDAO.instance == null) {
+            SQLEnseignantDAO.instance = new SQLEnseignantDAO();
         }
 
-        return SQLDepartementDAO.instance;
+        return  SQLEnseignantDAO.instance;
     }
 
     private MySQLDAOFactory daoFactory;
 
-    private SQLDepartementDAO() {
+    private  SQLEnseignantDAO() {
 
         this.daoFactory = new MySQLDAOFactory();
     }
 
     @Override
-    public ArrayList<Departement> readAll() {
+    public ArrayList<Enseignant> readAll() {
 
-        ArrayList<Departement> listeDepartement = new ArrayList<Departement>();
+        ArrayList<Enseignant> listeEnseignant = new ArrayList<Enseignant>();
 
         Connection connection = this.daoFactory.getConnexion();
-        String sql = "SELECT * FROM departement ORDER BY id_dpt ASC";
+        String sql = "SELECT * FROM enseignant ORDER BY id_dpt ASC";
 
         try {
             Statement st = connection.createStatement();
             ResultSet res = st.executeQuery(sql);
             while (res.next()) {
-                Departement departement = new Departement(res.getInt("id_dpt"), res.getString("libelle_dpt"));
-                listeDepartement.add(departement);
+                Enseignant enseignant = new Enseignant(res.getInt("id_ens"), res.getString("nom_ens"), res.getString("prenom_ens"));
+                listeEnseignant.add(enseignant);
             }
         } catch (SQLException se) {
             System.out.println("Erreur rq sql : " + se.getMessage());
@@ -51,45 +53,47 @@ public class SQLDepartementDAO implements DAOInterface<Departement> {
             this.daoFactory.fermeConnexion();
         }
 
-        return listeDepartement;
+        return listeEnseignant;
     }
 
     @Override
-    public int create(Departement departement) {
+    public int create(Enseignant enseignant) {
         Connection connection = this.daoFactory.getConnexion();
-        String insert = "INSERT INTO departement SET libelle_dpt = ?";
+        String insert = "INSERT INTO enseignant SET nom_ens = ? and prenom_ens = ?";
 
         int id = -1;
-
+/*******************QUE SIGNIFIE LE STI ET A QUOI IL SERT*****************/
         try {
             PreparedStatement stI = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stI.setString(1, departement.getLibelleDepartement());
+            stI.setString(1, enseignant.getNomEnseignant());
             stI.executeUpdate();
 
             ResultSet cle = stI.getGeneratedKeys();
             if (cle.next()) {
                 id = cle.getInt(1);
-                departement.setIdDepartement(id);
+                enseignant.setIdEnseignant(id);
             }
             stI.close();
         } catch (SQLException se) {
             System.out.println("Erreur rq sql : " + se.getMessage());
         }
-         return id;
+
+        return id;
     }
 
     @Override
-    public boolean update(Departement departement) {
+    public boolean update(Enseignant enseignant) {
         Connection connection = this.daoFactory.getConnexion();
-        String update = "UPDATE departement SET libelle_dpt = ? WHERE id_dpt= ?";
+        String update = "UPDATE departement SET nom_ens = ?, prenom_ens WHERE id_ens= ?";
 
         boolean updated = false;
 
         try {
             PreparedStatement stU = connection.prepareStatement(update);
 
-            stU.setString(1, departement.getLibelleDepartement());
-            stU.setInt(2, departement.getIdDepartement());
+            stU.setString(1, enseignant.getNomEnseignant());
+            stU.setString (2, enseignant.getPrenomEnseignant());
+            stU.setInt(3, enseignant.getIdEnseignant());
             stU.execute();
 
             updated = stU.getUpdateCount() > 0;
@@ -100,11 +104,11 @@ public class SQLDepartementDAO implements DAOInterface<Departement> {
 
         return updated;
     }
-
+/*
     @Override
-    public boolean delete(Departement departement) {
+    public boolean delete(Enseignant enseignant) {
         Connection connection = this.daoFactory.getConnexion();
-        String delete = "DELETE FROM departement where id_dpt = ?";
+        String delete = "DELETE FROM enseignant where id_ens = ?";
 
         boolean deleted = false;
 
@@ -130,5 +134,5 @@ public class SQLDepartementDAO implements DAOInterface<Departement> {
             this.delete(departement);
         }
     }
-
+*/
 }
